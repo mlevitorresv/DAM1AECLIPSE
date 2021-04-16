880b3aeb47a63b3dc46aebc7529837b4f33f4174
Manuel Leví <manuell.torvic@educa.jcyl.es>
2021-04-16 12:24:13 +0200

    Validación en cambio1

--- Prueba1/src/principal/Principal.java
+++ Prueba1/src/principal/Principal.java
@@ -7,6 +7,7 @@ public class Principal {
 		System.out.println("Hola Mundo Proyecto GIT-Java");
 		System.out.println("Hola de nuevo");
 		System.out.println("Cambio en cambio 1");
+		System.out.println("Nuevo cambio en cambio1");
 	}
 
 }
