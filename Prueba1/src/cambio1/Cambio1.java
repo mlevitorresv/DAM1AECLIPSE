90192a9147a11660ed34d4a3a4c7017e3940cc08
Manuel Leví <manuell.torvic@educa.jcyl.es>
2021-04-21 13:42:54 +0200

    validacion de la clase cambio1 en cambio1

---
+++ Prueba1/src/cambio1/Cambio1.java
@@ -0,0 +1,13 @@
+package cambio1;
+
+public class Cambio1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hola en cambio1");
+		System.out.println("Hola de nuevo en cambio1");
+		System.out.println();
+
+	}
+
+}
